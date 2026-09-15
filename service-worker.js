f1bf00b56b49f191428535b408bb27303a788961
mdicka22 <mdikaandrian@gmail.com>
2026-09-15 09:34:42 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='portal-rumah-v9';
+const CACHE_NAME='portal-rumah-v10';
 const APP_SHELL=['./','./index.html','./app.js','./styles.css','./document-preview.css','./login-blend.css','./ios-pwa.css','./assets/login-interior-coral.png','./assets/apple-touch-icon.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
